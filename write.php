49de2e3d8a6016d0e9ad4a97aaf6ddf6149a0793
IBinLakpa <107236919+IBinLakpa@users.noreply.github.com>
2023-03-02 23:11:45 +0545

    Update write.php

--- write.php
+++ write.php
@@ -1,7 +1,7 @@
 <?php
    @include 'config.php';
    session_start();
-   if(!isset($_SESSION['username'])){
+   if(!isset($_SESSION['id'])){
       header('location:login_form.php');
    }
    if(isset($_POST['submit'])){
